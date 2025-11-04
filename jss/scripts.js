326a0edfe4edd563cc067bd9a59fdb2e666ebffb
Petros Belachew <petrosbelachew@gmail.com>
2025-11-04 23:41:26 +0300

    fix: correct the hair button syle

--- jss/scripts.js
+++ jss/scripts.js
@@ -6,3 +6,16 @@ if (hairButton) {
     hairButton.classList.toggle(newClassName);
   });
 }
+
+// or use the check,add, remove
+
+// // Check if the class is ALREADY on the button
+//     if (hairButton.classList.contains(newClassName)) {
+//         // If it's there, remove it (toggling)
+//         hairButton.classList.remove(newClassName);
+//         console.log(`Class "${newClassName}" removed.`);
+//     } else {
+//         // If it's NOT there, add the new class
+//         hairButton.classList.add(newClassName);
+//         console.log(`Class "${newClassName}" added.`);
+//     }
