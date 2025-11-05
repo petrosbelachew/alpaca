d5596ef6f96e8dbf66aa9031b5c2583c21cbafd7
Petros Belachew <petrosbelachew@gmail.com>
2025-11-05 11:55:26 +0300

    feat: add hair styles

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,9 +1,28 @@
 const hairButton = document.getElementById("hair");
-const newClassName = "buttonclicked";
+const harClass = "buttonclicked";
+
+const hairStyles = document.getElementsByClassName("hair-styles-hidden");
+const hairStylesVisible = "hair-styles-visible";
+const hairStyleHidden = "hair-styles-hidden";
 
 if (hairButton) {
   hairButton.addEventListener("click", function () {
-    hairButton.classList.toggle(newClassName);
+    hairButton.classList.toggle(harClass);
+    // Use classList.remove() to make it visible
+    // Loop through every element in the collection
+    for (let i = 0; i < hairStyles.length; i++) {
+      const element = hairStyles[i];
+
+      // 1. Remove the 'hidden' class
+      element.classList.remove(hairStyleHidden);
+
+      // 2. Add the 'visible' class
+      element.classList.toggle(hairStylesVisible);
+
+      // NOTE: A cleaner approach is to use toggle() on both
+      // element.classList.toggle(hairStyleHidden);
+      // element.classList.toggle(hairStylesVisible);
+    }
   });
 }
 
